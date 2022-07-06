fcc7f86a8abcc4977085183fb722eb4d01dbafeb
Dmytro M <dmytromarzhiievskyi@gmail.com>
2022-07-06 13:38:36 +0300

    added controller (mainController)

--- src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/constants/Strings.java
+++ src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/constants/Strings.java
@@ -1,16 +1,16 @@
 package ru.javarush.cryptoanalyzer.marzhiievskyi.constants;
 
 public class Strings {
-    private final String engAlphabet = "qwertyuiopasdfghjklzxcvbnm";
-    private final String rusAlphabet = "йцукенгшщзхъфывапролджэячсмитьбюё";
-    private final String ukrAlphabet = "йцукенгшщзхїфівапролджєячсмитьбю";
-    private final String numeric = "0123456789";
-    private final String symbols = ".,/?'\"[];:!@#$%^&*()";
+    private static final String ENG_ALPHABET = "qwertyuiopasdfghjklzxcvbnm";
+    private static final String RUS_ALPHABET = "йцукенгшщзхъфывапролджэячсмитьбюё";
+    private static final String UKR_ALPHABET = "йцукенгшщзхїфівапролджєячсмитьбю";
+    private static final String NUMERIC = "0123456789";
+    private static final String SYMBOLS = ".,/?'\"[];:!@#$%^&*()";
 
     // all symbols what we are going to use for crypto analyzer
-    public final String allSymbols =
-            engAlphabet + engAlphabet.toUpperCase()
-            + rusAlphabet + rusAlphabet.toUpperCase() + ukrAlphabet
-            + ukrAlphabet.toUpperCase() + numeric + symbols;
+    public static final String ALL_SYMBOLS =
+            ENG_ALPHABET + ENG_ALPHABET.toUpperCase()
+            + RUS_ALPHABET + RUS_ALPHABET.toUpperCase() + UKR_ALPHABET
+            + UKR_ALPHABET.toUpperCase() + NUMERIC + SYMBOLS;
 
 }
--- src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/controller/MainController.java
+++ src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/controller/MainController.java
@@ -1,10 +1,8 @@
 package ru.javarush.cryptoanalyzer.marzhiievskyi.controller;
 
-import java.util.Arrays;
-
 public class MainController {
     public void execute(String command, String[] parameters) {
-       
+
     }
 
 }
