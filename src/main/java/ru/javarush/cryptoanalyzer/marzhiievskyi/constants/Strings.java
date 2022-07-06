1724b116b1b711d11facb0f1897c11c47b996114
Dmytro M <dmytromarzhiievskyi@gmail.com>
2022-07-06 13:43:44 +0300

    added space in Strings and correct gitignore

--- src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/constants/Strings.java
+++ src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/constants/Strings.java
@@ -5,7 +5,7 @@ public class Strings {
     private static final String RUS_ALPHABET = "йцукенгшщзхъфывапролджэячсмитьбюё";
     private static final String UKR_ALPHABET = "йцукенгшщзхїфівапролджєячсмитьбю";
     private static final String NUMERIC = "0123456789";
-    private static final String SYMBOLS = ".,/?'\"[];:!@#$%^&*()";
+    private static final String SYMBOLS = ".,/?'\"[];:!@#$%^&*() ";
 
     // all symbols what we are going to use for crypto analyzer
     public static final String ALL_SYMBOLS =
