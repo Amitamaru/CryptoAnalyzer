f6e3ce155f74bc10b1954c752de4a786f7e763ce
Dmytro M <dmytromarzhiievskyi@gmail.com>
2022-07-08 11:19:46 +0300

    modified Strings checked for name mistakes =)

--- src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/constants/Strings.java
+++ src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/constants/Strings.java
@@ -2,12 +2,12 @@ package ru.javarush.cryptoanalyzer.marzhiievskyi.constants;
 
 public class Strings {
     private static final String ENG_ALPHABET = "qwertyuiopasdfghjklzxcvbnm";
-    private static final String CYRILIC_ALPHABET = "йцукенгшщзхъфывапролджэячсмитьбюёії";
+    private static final String CYRILLIC_ALPHABET = "йцукенгшщзхъфывапролджэячсмитьбюёії";
     private static final String NUMERIC = "0123456789";
     private static final String SYMBOLS = ".,/?'\"[];:!@#$%^&*() -";
 
     public static final String ALL_SYMBOLS =
             ENG_ALPHABET + ENG_ALPHABET.toUpperCase()
-            + CYRILIC_ALPHABET + CYRILIC_ALPHABET.toUpperCase() + NUMERIC + SYMBOLS;
+            + CYRILLIC_ALPHABET + CYRILLIC_ALPHABET.toUpperCase() + NUMERIC + SYMBOLS;
     //public static final char[] ALL_CHARS = ALL_SYMBOLS.toCharArray();
 }
