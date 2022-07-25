f7074025beb9fe21992d900a6bb990afaa97fd64
Dmytro M <dmytromarzhiievskyi@gmail.com>
2022-07-25 11:28:18 +0300

    updated to eng lang input text lord of the rings ch 1 dict text lord of the ring ch 2

--- src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/constants/Strings.java
+++ src/main/java/ru/javarush/cryptoanalyzer/marzhiievskyi/constants/Strings.java
@@ -12,7 +12,7 @@ public class Strings {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
 
-    private static final String CYRILLIC_ALPHABET = "йцукенгшщзхъфывапролджэячсмитьбюё";
+    private static final String CYRILLIC_ALPHABET = "qwertyuiopasdfghjklzxcvbnm";
     private static final String NUMERIC = "0123456789";
     private static final String SYMBOLS = "-.,?'\";:! ";
 
@@ -27,41 +27,41 @@ public class Strings {
         }
     }
     //regex pattern for ru dictionary
-    public static final String REGEX_FINDER_PATTERN = "(\\. [А-Я])|(, но)|(, не)";
+    public static final String REGEX_FINDER_PATTERN = "(\\. [A-Z])|(, which)|(, but)|(, and)";
 
     //dialog msg's for program
     private static final String EXIT_WORD = ANSI_GREEN + "exit" + ANSI_RESET;
-    public static final String WELCOME_MESSAGE = ANSI_BLUE + "\nДобро пожаловать в Шифр Цезаря. Выберете и введите номер команды, " +
-            "или слово " + EXIT_WORD + ANSI_BLUE + " для выхода из программы." + ANSI_RESET;
+    public static final String WELCOME_MESSAGE = ANSI_BLUE + "\nWelcome. Please enter command number, " +
+            "or enter " + EXIT_WORD + ANSI_BLUE + " to exit from program" + ANSI_RESET;
     public static final String CHOSE_COMMAND = ANSI_CYAN + """
 
-            Выберете действие :\s
-            Введите 1 чтобы зашифровать текст из файле
-            Введите 2 чтобы расшифровать текст из зашифрованного
-            Введите 3 чтобы расшифровать методом Brute Force
-            Введите 4 чтобы попробовать расшифровать статистическим анализом""" + ANSI_RESET;
+            Choose action :\s
+            Enter 1 to encrypt text from file
+            Enter 2 to decrypt text from encrypted file
+            Enter 3 to decrypt using method Brute Force
+            Enter 4 to try decrypt by text analyzing""" + ANSI_RESET;
 
-    public static final String ENTER_KEY_SHIFT = ANSI_PURPLE + "Введите цифру-ключ шифрования" + ANSI_RESET;
-    public static final String ENTER_ENCRYPTED_TEXT_FILE = ANSI_PURPLE + "Введите имя файла, на котором зашифрован текст" + ANSI_RESET;
-    public static final String ENTER_INPUT_FILE_TEXT = ANSI_PURPLE + "Введите имя файла с текстом для шифрования" + ANSI_RESET;
-    public static final String ENTER_OUT_FILE_NAME = ANSI_PURPLE + "Введите имя файла, в котором будет результат" + ANSI_RESET;
-    public static final String ENTER_INPUT_DICTIONARY_TEXT_FILE = ANSI_PURPLE + "Введите имя файла, в котором текст того же автора, для анализа" + ANSI_RESET;
-    public static final String GOOD_BYE = ANSI_GREEN + "До свидания" + ANSI_RESET;
-    public static final String IO_EXCEPTION_MSG = ANSI_RED + "Ошибка пути файла" + ANSI_RESET;
-    public static final String ARGS_EXCEPTION_MSG = ANSI_RED + "Некорректно заданы аргументы" + ANSI_RESET;
-    public static final String KEY_EXCEPTION_MSG = ANSI_RED + "Значение ключа не может быть '0', или отрицательным" + ANSI_RESET;
+    public static final String ENTER_KEY_SHIFT = ANSI_PURPLE + "Enter key of encryption" + ANSI_RESET;
+    public static final String ENTER_ENCRYPTED_TEXT_FILE = ANSI_PURPLE + "Enter file name with encrypted text" + ANSI_RESET;
+    public static final String ENTER_INPUT_FILE_TEXT = ANSI_PURPLE + "Enter file name with incoming original text" + ANSI_RESET;
+    public static final String ENTER_OUT_FILE_NAME = ANSI_PURPLE + "Enter file name for result" + ANSI_RESET;
+    public static final String ENTER_INPUT_DICTIONARY_TEXT_FILE = ANSI_PURPLE + "Enter the name of the file with similar text to analyze" + ANSI_RESET;
+    public static final String GOOD_BYE = ANSI_GREEN + "Good by" + ANSI_RESET;
+    public static final String IO_EXCEPTION_MSG = ANSI_RED + "File path error" + ANSI_RESET;
+    public static final String ARGS_EXCEPTION_MSG = ANSI_RED + "Incorrect arguments" + ANSI_RESET;
+    public static final String KEY_EXCEPTION_MSG = ANSI_RED + "The key can not be '0', or be negative" + ANSI_RESET;
 
     //system msg's for program
-    public static final String MSG_ENCRYPTION_DONE_WELL = "Шифрование завершено. ";
-    public static final String MSG_PATH_TO_THE_RESULT = "\nПуть к результату: ";
-    public static final String MSG_DECRYPTION_DONE_WELL = "Расшифрование завершено. ";
-    public static final String MSG_DECRYPTION_DONE_FAILED = "Расшифрование закончено неудачно";
-    public static final String MSG_DECRYPTION_BRUTE_FORCE_DONE_WELL = "Расшифрование закончено удачно. Ключ шифрования должен быть: ";
-    public static final String MSG_DECRYPTION_BY_STATISTIC_ANALYZE_FINISHED = "Расшифрование закончено. Проверте результат.";
-    public static final String MSG_EXCEPTION_FAILED = "Неудача. ";
+    public static final String MSG_ENCRYPTION_DONE_WELL = "Encryption finished. ";
+    public static final String MSG_PATH_TO_THE_RESULT = "\nPath to the result: ";
+    public static final String MSG_DECRYPTION_DONE_WELL = "Decryption finished. ";
+    public static final String MSG_DECRYPTION_DONE_FAILED = "Decryption failed";
+    public static final String MSG_DECRYPTION_BRUTE_FORCE_DONE_WELL = MSG_DECRYPTION_DONE_WELL + "The key should be: ";
+    public static final String MSG_DECRYPTION_BY_STATISTIC_ANALYZE_FINISHED = MSG_DECRYPTION_DONE_WELL + "Please check result.";
+    public static final String MSG_EXCEPTION_FAILED = "Failed. ";
 
     //result msg's
-    public static final String RESULT_OF_PROGRAM = "Результат работы программы: ";
-    public static final String CODE_OF_RESULT = "\nкод результата = ";
-    public static final String MESSAGE_WORD = "\nсообщение = ";
+    public static final String RESULT_OF_PROGRAM = "The result of program: ";
+    public static final String CODE_OF_RESULT = "\ncode of result = ";
+    public static final String MESSAGE_WORD = "\nmessage = ";
 }
